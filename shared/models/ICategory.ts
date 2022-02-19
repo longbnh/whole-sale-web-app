40ec8a37a5774bfb80c5d671221272842b1802c6
Bui Nguyen Hoang Long <bhoanglong@gmail.com>
2022-02-19 22:40:25 +0700

    handle paging category

--- shared/models/ICategory.ts
+++ shared/models/ICategory.ts
@@ -1,12 +1,15 @@
 export interface ISubCategory {
   id: number;
   name: string;
+  imageUrl: string;
   priority: number;
 }
 
 interface ICategory {
   name: string;
   priority: number;
+  imageUrl: string;
+  iconUrl: string;
   subCategories: Array<ISubCategory>;
 }
 
