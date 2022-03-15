e8ab2efba208936e6d10e4438f7f3872ab46cd5c
heroziber <heroziber@gmail.com>
2022-03-15 21:54:34 +0700

    fix campaign display of hidden status from 1 to 2

--- shared/enum/enum.ts
+++ shared/enum/enum.ts
@@ -104,7 +104,7 @@ export namespace PAGE_REQUEST {
       }
       export enum CAMPAIGN_DISPLAY {
         ACTIVE = 0,
-        HIDDEN = 1,
+        HIDDEN = 2,
         COMPLETE = 3,
       }
     }
